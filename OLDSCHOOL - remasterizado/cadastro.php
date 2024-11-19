748f460077359462a91b7c83e9c1c906538f4d10
petrus <167083540+Alecsander2005@users.noreply.github.com>
2024-11-19 14:24:04 -0300

    Update cadastro.php

--- OLDSCHOOL - remasterizado/cadastro.php
+++ OLDSCHOOL - remasterizado/cadastro.php
@@ -2,17 +2,22 @@
 include('conexao.php');
 
 //** Verificando se o formulário foi enviado */
-if (isset($_POST['email']) && isset($_POST['senha'])) {
+if (isset($_POST['email']) && isset($_POST['senha']) && isset($_POST['telefone'])) {
 
     //** verificando se os campos de email e senha não estao vazios */
     if (strlen($_POST['email']) == 0) {
         echo "Preencha seu e-mail";
     } else if (strlen($_POST['senha']) == 0) {
         echo "Preencha sua senha";
-    } else {
-        //** limpando os dados */
+    }
+    else if(strlen($_POST['telefone']) == 0){
+        echo "preencha seu telefone";
+    }
+    else {
+    //** limpando os dados */
         $mail = $mysqli->real_escape_string($_POST['email']);
         $senha = $_POST['senha'];
+        $tel = $_POST['telefone'];
 
         //**Se o e-mail não existir, criamos um novo usuário */
         $sql_code = "SELECT * FROM usuarios WHERE email = '$mail'";
@@ -25,7 +30,7 @@ if (isset($_POST['email']) && isset($_POST['senha'])) {
             //** Se o e-mail não existir, criamos um novo usuário */ 
 
             // Inserindo o novo usuário no banco de dados
-            $sql_code = "INSERT INTO usuarios (email, senhas) VALUES ('$mail', '$senha')";
+            $sql_code = "INSERT INTO usuarios (email, senhas, telefone) VALUES ('$mail', '$senha', '$tel')";
             if ($mysqli->query($sql_code)) {
                 echo "Cadastro realizado com sucesso! <a href='login.php'>Clique aqui para fazer login.</a>";
             } else {
@@ -51,8 +56,10 @@ if (isset($_POST['email']) && isset($_POST['senha'])) {
         </p>
         <p>
             <label for="senha">Senha</label>
-            <input type="password" name="senha" id="senha" required>
+            <input type="password" name="senha" id="senha" maxlength="16" required>
         </p>
+            <label for="telefone">telefone</label>
+            <input type="tel" name="telefone" id="telefone" maxlength="11" required>
         <p>
             <button type="submit">Cadastrar</button>
         </p>
